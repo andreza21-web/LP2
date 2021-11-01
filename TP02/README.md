60f0edeb9686a5fc3a6cd8003d2159a6220a1435
Andreza do Nascimento Pessoa <79468757+andreza21-web@users.noreply.github.com>
2021-11-01 17:58:06 -0300

    TP02- interface

---
+++ TP02/README.md
@@ -0,0 +1,11 @@
+# LINGUAGEM DE PROGRAMAÇÃO II LP2I4  - TP02
+
+## Integrantes
+
+Andreza Pessoa  Matricula: Cb3012191
+
+Lidiane Souza Soares Matricula: Cb3012026
+
+## Link do Video
+
+https://youtu.be/pNYG8tOawDc
---
+++ TP02/src/Aluno.java
@@ -0,0 +1,51 @@
+import java.util.UUID;
+
+/**
+ * @version 1.0
+ * @author Andreza Pessoa  Cb3012191
+ * @author Lidiane Soare  CB3012026
+ * 
+ */
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String endereco;
+    private UUID uuid;
+
+      public Aluno(String nome, int idade, String endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.uuid = UUID.randomUUID();
+    }
+
+    
+    public String getEndereco() {
+        return endereco;
+    }
+     public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+     public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+     public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    } 
+     public void setUUID(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+}
